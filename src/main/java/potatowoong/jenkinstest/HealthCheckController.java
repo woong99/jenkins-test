d655a0362fbe6969f0187a4d27cc45069a130709
woong99 <wg.jeong@springworks.co.kr>
2024-05-26 19:43:18 +0900

    test

--- src/main/java/potatowoong/jenkinstest/HealthCheckController.java
+++ src/main/java/potatowoong/jenkinstest/HealthCheckController.java
@@ -1,11 +1,13 @@
 package potatowoong.jenkinstest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
 
+    @GetMapping("/")
     public String healthCheck() {
         return "I'm healthy11!";
     }
